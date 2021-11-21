d5f914b2da81bfc2e0bea748bf27131637e4f041
hatoo <hato2000@gmail.com>
2021-11-22 07:55:16 +0900

    tweak

--- raytracing-example-plus/src/main.rs
+++ raytracing-example-plus/src/main.rs
@@ -1935,7 +1935,7 @@ fn sample_scene(
     world.push((
         vk::AccelerationStructureInstanceKHR {
             transform: vk::TransformMatrixKHR {
-                matrix: [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.2],
+                matrix: [4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, -1.0, 1.2],
             },
             instance_custom_index_and_mask: 0xff << 24,
             instance_shader_binding_table_record_offset_and_flags:
